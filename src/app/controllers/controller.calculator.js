50375151a2394a76f0159e14962afbacc8650d85
Johnny <johnnymaikeo@gmail.com>
2017-07-13 11:14:06 -0300

    fully functiona calculator, next looking for exception scenarios

--- src/app/controllers/controller.calculator.js
+++ src/app/controllers/controller.calculator.js
@@ -6,12 +6,19 @@ define(['app'], function (app) {
   function CalculatorController() {
 
     var vm = this;
-    vm.display = '0';
+    vm.display = '1+((2+2)+(3+3))';
     vm.upperline = '';
     vm.buttonPressClear = buttonPressClear;
     vm.buttonPressEval = buttonPressEval;
     vm.buttonPress = buttonPress;
     vm.keyPress = keyPress;
+
+    var calculator = {
+      '+': function (a, b) { return a + b },
+      '-': function (a, b) { return a - b },
+      '/': function (a, b) { return a / b },
+      '*': function (a, b) { return a * b }
+    }
 
     ////////////////
 
@@ -24,6 +31,10 @@ define(['app'], function (app) {
         vm.display = '';
         vm.upperline = '';
       }
+      if (vm.display == '' && value == '00') {
+        vm.display = '0'
+      }
+    
       if (vm.display.length == 10) {
         error('max 10 caracters');
         return
@@ -52,7 +63,13 @@ define(['app'], function (app) {
     }
 
     function buttonPressEval() {
-      console.log('buttonPressEval');
+      const regex = /\([^()"]*(?:"[^"]*"[^()"]*)*\)/;
+      var m;
+      vm.upperline = vm.display;
+      while (m = regex.exec(vm.display)) {
+        execute(m[0]);
+      }
+      execute(vm.display);
     }
 
     ////////////////
@@ -60,6 +77,45 @@ define(['app'], function (app) {
     function error(message) {
         vm.display = 'ERROR';
         vm.upperline = message;
+    }
+    
+    function execute(expression) {
+      var result = expressionEval(expression);
+      if (result) {
+        vm.display = vm.display.replace(expression, result);
+      } else {
+        error('Invalid expression evaluated');
+      }
+    }
+
+    function expressionEval(expression) {
+      const regex = /([\(\)])/g;
+      expression = expression.replace(regex, '');
+      const explode = /([+-/*])/gi;
+      var array = expression.split(explode);
+      var i, previous, next, result;
+      if (array.length == 2) {
+        return null;
+      } else if (array.length == 1) {
+        return array[0];
+      }
+
+      for (var operator in calculator) {
+        if(calculator.hasOwnProperty(operator) ) {
+          while (array.indexOf(operator) >= 0) {
+            i = array.indexOf(operator);
+            previous = parseFloat(array[i-1]);
+            next = parseFloat(array[i+1]);
+            if (!isNaN(previous) && !isNaN(next)) {
+              var result = calculator[operator](previous, next);
+              array.splice(i-1, 3, result);
+            } else {
+              return null;
+            }
+          }
+        } 
+      }
+      return array[0];
     }
 
     activate();
